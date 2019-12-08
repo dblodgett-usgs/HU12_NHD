d6744d2267a2b9a15f1a66c39a72cd7d3c239288
David Blodgett <dblodgett@usgs.gov>
2019-12-07 21:18:26 -0600

    yeti

--- sourcer.R
+++ sourcer.R
@@ -1,27 +1,41 @@
+try(system("module purge"))
+try(system("module load gdal/2.4.1-gcc71 gcc/7.1.0 gis/geos-3.5.0 module load proj/4.9.2-gcc6.1.0 tools/udunits-2.2.16-gnu"))
+
 cran_packages <- c("sf", "dplyr", "drake", "snow", "xml2", 
-                   "readr", "rmapshaper", "R.utils", "tidyr",
-                   "igraph", "readr", "pbapply", "remotes")
+                   "readr", "R.utils", "tidyr", # "rmapshaper"
+                   "readr", "pbapply", "remotes")
 
 usgs_r_github_packages <- c("nhdplusTools", "HUCAgg")
 
 local_package <-  "mainstems"
 
-packages <- c(cran_packages, usgs_r_github_packages, local_package)
+igraph <- "igraph"
+
+packages <- c(cran_packages, usgs_r_github_packages, local_package, igraph)
 
 inst <- installed.packages()
 
-if(any(!(inst_check <- cran_packages %in% inst[, 1]))) {
-  install.packages(cran_packages[inst_check])
-}
+if(!"devtools" %in% inst[, 1])
+  install.packages("devtools", repos = "https://cloud.r-project.org")
 
 if(any(!(ur_check <- usgs_r_github_packages %in% inst[, 1]))) {
-  lapply(usgs_r_github_packages[ur_check], 
+  lapply(usgs_r_github_packages[!ur_check], 
          function(x) remotes::install_github(paste0("usgs-r/", x)))
 }
 
+if(!"units" %in% inst[, 1]) 
+  install.packages('udunits2', configure.args='--with-udunits2-lib=/cxfs/projects/root/opt/udunits/2.2.16/gnu/lib --with-udunits2-include=/cxfs/projects/root/opt/udunits/2.2.16/gnu/include')
+
+if(!igraph %in% inst[, 1]) {
+  devtools::install_version('igraph', '1.2.2')
+}
+  
+if(any(!(inst_check <- cran_packages %in% inst[, 1]))) {
+  install.packages(cran_packages[!inst_check])
+}
 
 if(!local_package %in% inst[, 1]) {
-  install.packages(local_package)
+  install.packages(local_package, repos = NULL)
 }
 
 lapply(packages, require, character.only = TRUE)
