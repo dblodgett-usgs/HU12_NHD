6ef0faa896ef2bcc7fb9b206cbd3cfca4a45a290
David Blodgett <dblodgett@usgs.gov>
2019-12-14 20:39:38 -0600

    parallel run ready

--- hr_wbd_runner.R
+++ hr_wbd_runner.R
@@ -8,7 +8,7 @@ source("R/10_build_mainstems_table.R")
 hr_hu02 <- c("01", "02", "03", "07", "08", "05", "06", "10", 
         "11", "17", "12", "13", "14", "15", "16", "18")
 
-hr_hu02 <- c("10")
+# hr_hu02 <- c("10")
 
 hr_dir <- "data/hr/"
 out <- "nhdplushr_newwbd"
@@ -30,33 +30,37 @@ plan <- drake_plan(
   wbd = get_wbd(wbd_gdb_path, wbd_fixes, prj),
   hr_path = target(download_nhdplushr(hr_dir, hr_huset),
                       transform = map(hr_huset = !!hr_hu02)),
-  hr_net = target(get_nhdplushr(hrpth, 
+  hr_net = target(get_nhdplushr(hr_path, 
                                !!file.path(hr_dir, sprintf("%s.gpkg", .id_chr)), 
                                layers = "NHDFlowline",
                                pattern = ".*[0-9][0-9][0-9][0-9].*.gdb$", 
                                min_size_sqkm = min_size_sqkm, simp = proc_simp, 
                                proj = prj, check_terminals = TRUE),
-                     transform = map(hrpth)),
+                     transform = map(hr_path, .id = FALSE)),
   hu_joiner = target(par_match_levelpaths(hr_net, wbd, proc_simp, 1, temp_dir, 
                                           !!file.path(out, sprintf("joiner_%s.csv", .id_chr))),
-                     transform = map(hr_net, prep_data), hpc = TRUE),
+                     transform = map(hr_net, prep_data, .id = FALSE), hpc = TRUE),
   lp_points = target(get_lp_points(hu_joiner, hr_net, wbd, wbd_exclusions),
-                     transform = map(hu_joiner, hr_net), hpc = TRUE),
+                     transform = map(hu_joiner, hr_net, .id = FALSE), hpc = TRUE),
   na_ol = target(get_na_outlets_coords(lp_points, hr_net),
-                            transform = cross(lp_points), hpc = TRUE),
+                            transform = map(lp_points, .id = FALSE), hpc = TRUE),
   in_list = target(get_in_list(lp_points, hr_net),
-                   transform = cross(lp_points), hpc = TRUE),
+                   transform = map(lp_points, .id = FALSE), hpc = TRUE),
   lp = target(get_linked_points_scalable(in_list, na_ol, cores, !!file.path(out, sprintf("%s.gpkg", hr_hu02))),
-              transform = map(in_list, na_ol, hr_hu02 = !!hr_hu02), hpc = TRUE),
+              transform = map(in_list, na_ol, hr_hu02 = !!hr_hu02, .id = FALSE), hpc = TRUE),
   write = target(write_output_gpkg(hr_net, wbd, hu_joiner, lp, prj, 30,
                                    file_out(!!file.path(out, sprintf("%s.gpkg", hr_hu02)))),
-                 transform = map(hr_net, hu_joiner, lp, hr_hu02 = !!hr_hu02), hpc = TRUE)
+                 transform = map(hr_net, hu_joiner, lp, hr_hu02 = !!hr_hu02,.id = FALSE), hpc = TRUE)
 )
 
 
 config <- drake_config(plan = plan,
                        memory_strategy = "autoclean",
-                       garbage_collection = TRUE)
+                       garbage_collection = TRUE,
+                       parallelism = "future", 
+                       jobs = 8)
+
+future::plan(future::multiprocess)
 
 make(config = config)
 
@@ -69,7 +73,7 @@ names(all_outlets)[1] <- "NHDPlusID"
 write_sf(all_outlets, file.path(out, "all_outlets.gpkg"))
 
 all_joiner <- do.call(rbind, lapply(hr_hu02, function(x) {
-  read_csv(file.path(out, paste0("joiner_hu_joiner_hr_net_hrpth_", x, ".csv")))
+  read_csv(file.path(out, paste0("joiner_hu_joiner_hr_net_hr_path_", x, ".csv")))
 }))
 
 all_joiner <- write_csv(all_joiner, file.path(out, "all_joiner.csv"))
